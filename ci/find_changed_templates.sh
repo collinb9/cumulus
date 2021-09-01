7a70022a5584dd70645814ff55d9114803e2b7f4
Brendan Collins <58670985+collinb9@users.noreply.github.com>
2021-09-01 09:12:02 +0100

    Implement CI/CD pipeline (#7)
    implement CI/CD pipeline

--- ci/find_changed_templates.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export CHANGED_TEMPLATES=`git diff --name-only --diff-filter=d origin/master | grep -E 'template.yaml' | tr '\n' ' '`
-# if [ -z "$CHANGED_TEMPLATES" ]
-# then
-# 	export CHANGED_TEMPLATES="hello_world"
-# fi
