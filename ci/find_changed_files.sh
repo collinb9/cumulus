043766e9b93ee625336f169fbe0b668c0ebdb499
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-01 05:52:43 +0100

    find branch name

--- ci/find_changed_files.sh
+++ ci/find_changed_files.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$TRAVIS_BRANCH" ]
+then
+    echo "No TRAVIS_BRANCH, using existing BRANCH"
+else
+    export BRANCH=$TRAVIS_BRANCH
+fi
+echo $BRANCH
 export CHANGED_FILES=`git diff --name-only --diff-filter=d origin/${BRANCH} | tr '\n' ' '`
 export CHANGED_TEMPLATES=`git diff --name-only --diff-filter=d origin/${BRANCH} | grep -E 'template.yaml' | tr '\n' ' '`
