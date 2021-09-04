7bba0335b801ab2b04faa8991a2004ebe1aceab5
Brendan Collins <58670985+collinb9@users.noreply.github.com>
2021-09-04 13:02:59 +0200

    Debug travis deploy (#14)
    * change how aws credentials are treated

--- ci/aws_auth.sh
+++ ci/aws_auth.sh
@@ -2,30 +2,31 @@
 
 mkdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
-[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOL
+# cat > ~/.aws/credentials << EOL
+# [default]
+# aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+# aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+# EOL
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
 cat > ~/.aws/config << EOL
 [profile bastion]
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
-source_profile = default
 
 [profile staging]
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
-source_profile = default
 
 [profile prod]
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
-source_profile = default
 EOL
 
 ls ~/.aws
+
+aws configure list
