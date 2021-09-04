bd65cd9af6fe046bb2a698b2d75b6d085b40f9e5
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:40:43 +0100

    sudo

--- ci/aws_auth.sh
+++ ci/aws_auth.sh
@@ -2,109 +2,40 @@
 
 mkdir -p ~/.aws
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-cat > ~/.aws/credentials << EOL
-[default]
-<<<<<<< HEAD
-aws_access_key_id=${AWS_ACCESS_KEY_ID}
-aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
-
-=======
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
->>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
-EOL
-=======
-# cat > ~/.aws/credentials << EOL
-# [default]
-# aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-# aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-# EOL
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
->>>>>>> 7bba033 (Debug travis deploy (#14))
-=======
 cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 
 EOL
->>>>>>> fd9cda0 (write credentials to file)
 
 cat > ~/.aws/config << EOL
-<<<<<<< HEAD
-<<<<<<< HEAD
 [default]
 region = eu-west-1
 output = json
 role_arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
-=======
-[profile default]
-=======
-[default]
->>>>>>> 51aa57d (fix default config profile)
-region = eu-west-1
-output = json
-role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
->>>>>>> c71b993 (test)
 source_profile = default
 
 [profile bastion]
 region = eu-west-1
 output = json
-<<<<<<< HEAD
 role_arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
 source_profile = default
-=======
-role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
-<<<<<<< HEAD
->>>>>>> 7bba033 (Debug travis deploy (#14))
-=======
-source_profile = default
->>>>>>> 1f62da4 (add back source profile)
 
 [profile staging]
 region = eu-west-1
 output = json
-<<<<<<< HEAD
 role_arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
-=======
-role-arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
 source_profile = default
-=======
->>>>>>> 7bba033 (Debug travis deploy (#14))
-=======
-source_profile = default
->>>>>>> 1f62da4 (add back source profile)
 
 [profile prod]
 region = eu-west-1
 output = json
-<<<<<<< HEAD
 role_arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
-=======
-role-arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
 source_profile = default
 
-=======
->>>>>>> 7bba033 (Debug travis deploy (#14))
-=======
-source_profile = default
->>>>>>> 1f62da4 (add back source profile)
 EOL
 
 ls ~/.aws
 
-<<<<<<< HEAD
 aws configure list
-=======
-aws configure list
->>>>>>> 7bba033 (Debug travis deploy (#14))
