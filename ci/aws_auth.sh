1f62da4cb8c999e148fce04befac7f08b2e947e0
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 12:13:30 +0100

    add back source profile

--- ci/aws_auth.sh
+++ ci/aws_auth.sh
@@ -15,16 +15,19 @@ cat > ~/.aws/config << EOL
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
+source_profile = default
 
 [profile staging]
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
+source_profile = default
 
 [profile prod]
 region = eu-west-1
 output = json
 role-arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
+source_profile = default
 EOL
 
 ls ~/.aws
