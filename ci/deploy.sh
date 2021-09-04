d5b89e632a42a3e4b33b7d560090a8e61c998c83
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:23:26 +0100

    try this

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,8 +8,10 @@ export AWS_CONFIG_FILE="~/.aws/config"
 export AWS_SHARED_CREDENTIALS_FILE="~/.aws/credentials"
 aws --version
 which aws
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws s3 ls --profile staging
-cd iam/roles
-sam build --config-env $ENVIRONMENT
-sam deploy --config-env $ENVIRONMENT
-# python ci/apply_sam_workflow_merge.py
+# cd iam/roles
+# sam build --config-env $ENVIRONMENT
+# sam deploy --config-env $ENVIRONMENT
+python ci/apply_sam_workflow_merge.py
