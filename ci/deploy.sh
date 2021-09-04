38fda1e057f6aeb184c6f5df0df862a970257bc8
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:27:32 +0100

    adtry this

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -47,6 +47,7 @@ sam deploy --config-env $ENVIRONMENT
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws s3 ls --profile staging
+sudo aws s3 ls --profile staging
 # cd iam/roles
 # sam build --config-env $ENVIRONMENT
 # sam deploy --config-env $ENVIRONMENT
