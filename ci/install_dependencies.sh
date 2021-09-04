bd0d8edf8a803e124d4f223033dd9b88cbefda63
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:37:42 +0100

    remove sudo

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -3,7 +3,7 @@
 # AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip > /dev/null 2>&1
-sudo ./aws/install
+./aws/install
 which aws
 aws --version
 
