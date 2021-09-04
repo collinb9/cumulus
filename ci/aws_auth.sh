fd9cda05279a99d6dea35ea9e8a62a185919ce52
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:10:51 +0100

    write credentials to file

--- ci/aws_auth.sh
+++ ci/aws_auth.sh
@@ -2,13 +2,12 @@
 
 mkdir -p ~/.aws
 
-# cat > ~/.aws/credentials << EOL
-# [default]
-# aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-# aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-# EOL
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+
+EOL
 
 cat > ~/.aws/config << EOL
 [profile default]
