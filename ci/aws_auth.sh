cdb1359a60ffdceffb0dff94a8b3d23c49259349
Brendan Collins <58670985+collinb9@users.noreply.github.com>
2021-09-04 12:34:57 +0200

    Fix aws profiles in travis (#13)
    * fix roles in aws config file for travis

--- ci/aws_auth.sh
+++ ci/aws_auth.sh
@@ -4,9 +4,14 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
+<<<<<<< HEAD
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 
+=======
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+>>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
 EOL
 
 cat > ~/.aws/config << EOL
@@ -25,13 +30,21 @@ source_profile = default
 [profile staging]
 region = eu-west-1
 output = json
+<<<<<<< HEAD
 role_arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
+=======
+role-arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
+>>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
 source_profile = default
 
 [profile prod]
 region = eu-west-1
 output = json
+<<<<<<< HEAD
 role_arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
+=======
+role-arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
+>>>>>>> 4f5c3d4 (Fix aws profiles in travis (#13))
 source_profile = default
 
 EOL
