21d7444f2a4f5be20c07378efb2b9fc80203654f
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:49:04 +0100

    create roles in deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,6 +11,34 @@ aws --version
 which aws
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+cat > ~/.aws/config << EOL
+[default]
+region = eu-west-1
+output = json
+role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
+source_profile = default
+
+[profile bastion]
+region = eu-west-1
+output = json
+role-arn = arn:aws:iam::564188978527:role/IAMAdmin-bastion
+source_profile = default
+
+[profile staging]
+region = eu-west-1
+output = json
+role-arn = arn:aws:iam::626964907981:role/DeveloperAccess-staging
+source_profile = default
+
+[profile prod]
+region = eu-west-1
+output = json
+role-arn = arn:aws:iam::875094265107:role/DeveloperAccess-prod
+source_profile = default
+
+EOL
+
 aws s3 ls --profile staging
 aws sts get-caller-identity
 aws sts get-caller-identity --profile staging
