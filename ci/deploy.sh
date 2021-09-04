9c25efb922293a2264f7e723797208beb4f9c257
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 13:17:49 +0100

    export config and credentials location

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,6 +4,8 @@ export ENVIRONMENT=${1}
 
 echo "Deploying $ENVIRONMENT ..."
 
+export AWS_CONFIG_FILE="~/.aws/config"
+export AWS_SHARED_CREDENTIALS_FILE="~/.aws/credentials"
 aws --version
 which aws
 aws s3 ls --profile staging
