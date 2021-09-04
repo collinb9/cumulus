3644aa517b2c6aa91f82a097740d19b106f2710b
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-04 12:36:08 +0100

    fix

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,9 +5,13 @@ export ENVIRONMENT=${1}
 echo "Deploying $ENVIRONMENT ..."
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 aws sts get-caller-identity --profile $ENVIRONMENT
 =======
 aws -version
+=======
+aws --version
+>>>>>>> 5ee2619 (fix)
 which aws
 >>>>>>> 5d1ddcf (debug)
 python ci/apply_sam_workflow_merge.py
