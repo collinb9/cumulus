27f2f384832cabf8e43442b58137b1e5bca2d987
Brendan Collins <brendan.m.collins@outlook.com>
2021-09-09 19:51:23 +0100

    debug deploy

--- ci/apply_sam_workflow_pr.py
+++ ci/apply_sam_workflow_pr.py
@@ -7,6 +7,8 @@ CHANGED_FILES = os.environ.get("CHANGED_FILES", "")
 
 
 if __name__ == "__main__":
+    print(f"Environment: {ENVIRONMENT}")
+    print(f"Changed_files: {CHANGED_FILES}")
     changed_files = CHANGED_FILES.split()
     changed_sam_stacks = utils.find_sam_stacks_containing_files(changed_files)
     print("Changed stacks: ", changed_sam_stacks)
--- ci/test_sam_workflow_pr.py
+++ ci/test_sam_workflow_pr.py
@@ -7,8 +7,6 @@ CHANGED_FILES = os.environ.get("CHANGED_FILES", "")
 
 
 if __name__ == "__main__":
-    print(f"Environment: {ENVIRONMENT}")
-    print(f"Changed_files: {CHANGED_FILES}")
     changed_files = CHANGED_FILES.split()
     changed_sam_stacks = utils.find_sam_stacks_containing_files(changed_files)
     print("Changed stacks: ", changed_sam_stacks)
