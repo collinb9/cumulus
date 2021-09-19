b3a0d1f21937f8b0ebd334c4ede4af7437459ff4
collinb9 <brendan.m.collins@outlook.com>
2021-09-19 17:56:29 +0100

    Create artifacts bucket

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -7,6 +7,13 @@ sudo ./aws/install
 which aws
 aws --version
 
+# AWS SAM CLI
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+sha256sum aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d ~/sam-installation > /dev/null 2>&1
+sudo ~/sam-installation/install
+sam --version
+
 # cfn-nag
 gem install cfn-nag
 
