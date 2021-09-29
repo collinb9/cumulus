88938cb9a76ab6108ceaa763e42bb54c7b09ffa5
collinb9 <brendan.m.collins@outlook.com>
2021-09-29 03:55:25 +0100

    get basic functionality of lambda and firehose working

--- lake/ingest/understat/download_html/lambda_function.py
+++ lake/ingest/understat/download_html/lambda_function.py
@@ -3,9 +3,13 @@ import os
 import urllib.request as request
 import logging
 import json
+import uuid
 import logmatic
+import boto3
+import botocore
 
 LAMBDA_FUNCTION_NAME = os.environ.get("AWS_LAMBDA_FUNCTION_NAME")
+FIREHOSE_DELIVERY_STREAM = os.environ.get("FIREHOSE_DELIVERY_STREAM")
 
 LOGGER = logging.getLogger(LAMBDA_FUNCTION_NAME)
 LOGGER.propogate = False
@@ -15,6 +19,12 @@ LOG_LEVEL = os.environ.get("LOG_LEVEL")
 LOGGER.addHandler(LOG_HANDLER)
 LOGGER.setLevel(LOG_LEVEL)
 
+FIREHOSE = boto3.client("firehose")
+
+
+def put_kinesis_message(message: str):
+    pass
+
 
 def lambda_handler(event, context):
     """
@@ -23,7 +33,36 @@ def lambda_handler(event, context):
     LOGGER.debug(event)
     LOGGER.debug(context)
     url = json.loads(event["Records"][0]["body"])["url"]
-    response = request.urlopen(url)
-    html_page = response.fp.read()
+    # encapsulate in a function
+    with request.urlopen(url) as response:
+        html_page = response.read().decode(
+            response.headers.get_content_charset()
+        )
     payload = dict(url=url, html=html_page)
-    return payload
+    LOGGER.debug("payload", extra=payload)
+    # From this line down needs to be refined and put into a function
+    data = json.dumps(payload)
+    try:
+        response = FIREHOSE.put_record(
+            DeliveryStreamName=FIREHOSE_DELIVERY_STREAM,
+            Record=dict(Data=data),  # This needs to be compressed
+        )
+    except botocore.exceptions.ClientError as err:
+        LOGGER.error(
+            "Failed to put record to stream: %s",
+            FIREHOSE_DELIVERY_STREAM,
+            extra=dict(error=err),
+        )
+    else:
+        if response:
+            LOGGER.info(
+                "Message successfully sent to firehose stream %s",
+                FIREHOSE_DELIVERY_STREAM,
+                extra=response,
+            )
+        else:
+            LOGGER.warning(
+                "Encountered a problem trying to put record to firehose",
+                extra=response,
+            )
+    # return payload
