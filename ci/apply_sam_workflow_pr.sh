dbc08c64ecaa71110aeec26d0b5326c1f5b4b5d4
collinb9 <brendan.m.collins@outlook.com>
2021-11-23 17:32:57 +0000

    put branching logic in commands

--- ci/apply_sam_workflow_pr.sh
+++ ci/apply_sam_workflow_pr.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-git remote set-branches --add origin $TRAVIS_BRANCH
+branch="$1"
+
+git remote set-branches --add origin $branch
 git fetch
 source ci/find_changed_files.sh
 python ci/apply_sam_workflow_pr.py
