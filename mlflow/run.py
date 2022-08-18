45d74b0deaff0c025b18ab3d8b507b90732fa980
collinb9 <brendan.m.collins@outlook.com>
2022-08-18 05:03:29 +0000

    Deploy app with app runner

--- mlflow/run.py
+++ mlflow/run.py
@@ -1,10 +1,12 @@
 import mlflow
 
 def main():
-    remote_server_uri = "http://127.0.0.1:80"
+    remote_server_uri = "https://67iixhx8jn.eu-west-1.awsapprunner.com"
+    # remote_server_uri = "http://127.0.0.1:5000"
     mlflow.set_tracking_uri(remote_server_uri)
     # mlflow.set_experiment("/my-experiment")
     with mlflow.start_run():
+        print(mlflow.active_run().to_dictionary())
         mlflow.log_param("a", 1)
         mlflow.log_metric("b", 2)
         mlflow.log_artifact("run.py", "source")
